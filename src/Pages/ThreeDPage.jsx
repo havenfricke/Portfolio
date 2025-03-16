4fb8b7d2be0db47cb24c742e3be149604d37f3c4
havenf <havenfricke@u.boisestate.edu>
2025-03-15 21:20:37 -0600

    3d-viewer updates

--- src/Pages/ThreeDPage.jsx
+++ src/Pages/ThreeDPage.jsx
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ModelViewer from '../Components/ModelViewer';
 
 
 function ThreeDPage() {
-  // Array of model paths
+  // Array of model path
   const models = [
-    '/Portfolio/low_poly_starter_humanoid _TestA.obj',
-    '/Portfolio/low_poly_starter_humanoid _TestB .obj',
+    '/Portfolio/low_poly_starter_humanoid_TestA.obj',
+    '/Portfolio/low_poly_starter_humanoid_TestB.obj',
     '/Portfolio/low_poly_starter_humanoid.obj'
   ];
 
+  const [currentInfoText, setInfoText] = useState();
+
   const [currentModelIndex, setCurrentModelIndex] = useState(0);
+
+  useEffect (() => {
+    switch (models[currentModelIndex]) {
+      case '/Portfolio/low_poly_starter_humanoid_TestA.obj':
+        setInfoText("A simple humanoid .obj for sculpting, animating and prototyping. TestA");
+        break;
+      case '/Portfolio/low_poly_starter_humanoid_TestB.obj':
+        setInfoText("A simple humanoid .obj for sculpting, animating and prototyping. TestB");
+        break;
+      case '/Portfolio/low_poly_starter_humanoid.obj':
+        setInfoText("A simple humanoid .obj for sculpting, animating and prototyping.");
+        break;
+      default:
+        setInfoText("");
+    }
+  }, [currentModelIndex, models]);
 
   const handleNext = () => {
     setCurrentModelIndex((prev) => (prev + 1) % models.length);
@@ -22,15 +40,14 @@ function ThreeDPage() {
 
   return (
     <section className="three-d-page">
-      <ModelViewer  modelPath={models[currentModelIndex]}/>
+      <ModelViewer modelPath={models[currentModelIndex]}/>
         <div className="model-viewer-nav">
             <button className='back-viewer-nav' onClick={handlePrev}>&#8592; &nbsp; Back</button>
                 <span> {currentModelIndex + 1} / {models.length}</span>
             <button className='next-viewer-nav' onClick={handleNext}>Next &nbsp; &#8594;</button>
         </div>
         <div className='model-viewer-info'>
-            Please use my models responsibly &nbsp; &#8594;
-            <a href={models[currentModelIndex]}>&nbsp;&nbsp; download {models[currentModelIndex].replace("/Portfolio/", "")}</a>
+            {currentInfoText}
         </div>
     </section>
   );
