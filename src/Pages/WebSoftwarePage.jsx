48e3da7b148983ae01e095ce1c17c8b5a0061e37
havenf <havenfricke@u.boisestate.edu>
2025-03-31 16:31:08 -0600

    potfolio arifact additions, css changes

--- src/Pages/WebSoftwarePage.jsx
+++ src/Pages/WebSoftwarePage.jsx
@@ -24,7 +24,9 @@ function WebSoftwarePage()
                         <ModalWindow title={"Kendall Spokesmasters"}>
                             <div className='row'>
                                 <p>
-                                    The <a href="https://www.kendallautoidaho.com/" target='_blank'>Kendall</a> Spokemaster's marketing campaign micro-website was a project during employment at 
+                                    The <a href="https://www.kendallautoidaho.com/" target='_blank'>Kendall  
+                                    Spokemaster's </a> <a href="https://www.kendallautogroup.com/kendall-spokesmasters/" target="_blank">
+                                    marketing campaign</a> micro-website was a project during employment at 
                                     116 & West. Kendall Automotive Group, a client of 116 & West entrusted us with the creative work, 
                                     casting, filming, and additional marketing. My role was to build the website. Additional 
                                     responsibilities included testing, deploying to production, and maintenance.
@@ -54,10 +56,10 @@ function WebSoftwarePage()
                         <ModalWindow title={"Moffet Energy Modeling"}>
                             <div className='row'>
                                 <p>
-                                    <a href="https://www.moffetco.com/" target="_blank">Moffet Energy Modeling</a>, a local business in Boise and a client of <a href="https://116andwest.com/" target='_blank' >
-                                    116 & West Marketing Agency</a> during my employment with the company. The site was designed by a UI / UX specialist. 
-                                    My involvement was primarily translating the design into a website, the deployment, and maintenance
-                                    of the site once in production. 
+                                    <a href="https://www.moffetco.com/" target="_blank">Moffet Energy Modeling</a>, a local business in Boise and a client of 
+                                    <a href="https://116andwest.com/" target='_blank' > 116 & West Marketing Agency </a> during my employment with the company. 
+                                    The site was designed by a UI / UX specialist. My involvement was primarily translating the design into a website, 
+                                    the deployment, and maintenance of the site once in production. 
                                 </p>
                             </div>
                             <div className='row'>
@@ -84,7 +86,18 @@ function WebSoftwarePage()
                         <ModalWindow title={"React Vanilla Boilerplate"}>
                             <div className='row'>
                                 <p>
-                                    project description
+                                    This GitHub repository is boilerplate code that I made for beginning web applications while attending  
+                                    <a href="https://www.boisestate.edu/gimm/student-information/" target='_blank'> Boise State University</a>. 
+                                    The architecture was learned during my time as a student at <a href="https://boisecodeworks.com/" target='_blank'>Boise CodeWorks</a>, 
+                                    a trade school that focuses on teaching with refactored code to touch on basic concepts in a high 
+                                    level, full stack development enironment. It is equipped with vite's React starting application, 
+                                    react mobx, react document object model, react router, and axios. 
+                                    Additionally it has some example components, an application state, and API example call routes.
+                                    This is a client, single-page application, and front end of a full stack application. 
+                                    This website is using the boilerplate as a foundation.
+                                    <br />
+                                    <br />
+                                    <a href="https://github.com/havenf/ReactApp-Vanilla-Starter" target="_blank">GitHub Repository Link</a>
                                 </p>
                             </div>
                             <div className='row'>
