187bd15b7351eb5cec78c46954c019240ab78a4d
havenf <havenfricke@u.boisestate.edu>
2026-01-01 11:36:12 -0700

    removed book

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -54,7 +54,6 @@ function HomePage()
               <img className="book" alt="biocentrism" src="/Portfolio/biocentrism.jpg"/>
               <img className="book" alt="creativeact" src="/Portfolio/creativeact.jpg"/>
               <img className="book" alt="flow" src="/Portfolio/flow.jpg"/>
-              <img className="book" alt="vagabond" src="/Portfolio/vagabond.jpg"/>
               <img className="book" alt="techniqueofmaster" src="/Portfolio/techniqueofmaster.jpg"/>
               <img className="book" alt="alphabetgoddess" src="/Portfolio/alphabetgoddess.jpg"/>
               <img className="book" alt="taoteching" src="/Portfolio/taoteching.jpg"/>
