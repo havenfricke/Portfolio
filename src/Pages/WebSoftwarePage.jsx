69db7088a1c559903b18f465615f1aabb41d48a6
havenfricke <havenfricke@u.boisestate.edu>
2026-03-07 20:44:50 -0700

    removed unused imports

--- src/Pages/WebSoftwarePage.jsx
+++ src/Pages/WebSoftwarePage.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { observer } from 'mobx-react-lite';
 import ModalWindow from '../Components/ModalWindow'
-import { Skeleton } from '@mui/material';
 import LazyImage from '../Components/LazyImage';
 import MarkdownFile from '../Components/MarkdownFile';
 
