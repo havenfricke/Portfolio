9f90c0a7c7df7d9803cbfeebf69e3d13b0a58236
havenfricke <havenfricke@u.boisestate.edu>
2026-03-31 10:49:29 -0600

    moved games tab to bottom of list

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,12 +13,6 @@ export function Navbar() {
         <img className="right-arrow" src="/arrow-right-solid.svg" alt="Right Arrow" data-skip-on-tab="true"/>
       </button>
     </Link>
-    <Link to={'/gamedev'} className={location.pathname === '/gamedev' ? 'active' : ''} title="Game Dev Artifacts">
-      <button className={location.pathname === '/gamedev' ? 'active' : ''}>
-        <img className="game-pad" src="/gamepad-solid.svg" alt="Gamepad" data-skip-on-tab="true"/>
-        <img className="right-arrow" src="/arrow-right-solid.svg" alt="Right Arrow" data-skip-on-tab="true"/>
-      </button>
-    </Link>
     <Link to={'/websoftware'} className={location.pathname === '/websoftware' ? 'active' : ''} title="Web & Software Artifacts">
       <button className={location.pathname === '/websoftware' ? 'active' : ''}>
         <img className="code" src="/code-solid.svg" alt="Code Bracket" data-skip-on-tab="true"/>
@@ -37,6 +31,12 @@ export function Navbar() {
         <img className="right-arrow" src="/arrow-right-solid.svg" alt="Right Arrow" data-skip-on-tab="true"/>
       </button>
     </Link> 
+     <Link to={'/gamedev'} className={location.pathname === '/gamedev' ? 'active' : ''} title="Game Dev Artifacts">
+      <button className={location.pathname === '/gamedev' ? 'active' : ''}>
+        <img className="game-pad" src="/gamepad-solid.svg" alt="Gamepad" data-skip-on-tab="true"/>
+        <img className="right-arrow" src="/arrow-right-solid.svg" alt="Right Arrow" data-skip-on-tab="true"/>
+      </button>
+    </Link>
     {/* <Link to={'/join'} className={location.pathname === '/join' ? 'active' : ''} title="Join me">
       <button className={location.pathname === '/join' ? 'active' : ''}>
         <img className="door-open" src="/door-open-solid.svg" alt="Door Open" data-skip-on-tab="true"/>
